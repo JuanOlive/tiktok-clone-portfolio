5462c4e4af265db888936c1d15a54ca9ea2418c9
JuanOlive <juanpyramids@gmail.com>
2025-03-21 18:05:13 -0300

    mudei pra principal agfora é scuddarai

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -2,7 +2,7 @@ export default function explore() {
     return (
       <div className="flex justify-center">
         <h1 className="text-white">Bem-vindo à página inicial</h1>
-        <p>Esta é a página principal.</p>
+        <p>Esta é a página secundaria.</p>
       </div>
     );
   }
